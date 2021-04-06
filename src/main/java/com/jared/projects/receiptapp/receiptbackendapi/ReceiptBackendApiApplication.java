f28eba2da680b2d22fdc52978c15a9d7895eae8a
Jared York <jyorkdev@gmail.com>
2021-04-06 10:08:36 -0500

    initial commit

---
+++ src/main/java/com/jared/projects/receiptapp/receiptbackendapi/ReceiptBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.jared.projects.receiptapp.receiptbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jared/projects/receiptapp/receiptbackendapi/ReceiptBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jared.projects.receiptapp.receiptbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
